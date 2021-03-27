96e9509b5284538d6a1a0a7d42a3c140fcae11a5
Sara Roohan <sara.roohan@gmail.com>
2021-03-27 10:55:43 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Sara Roohan';
+var isCool = true;
+var totalPets = 0;
+var unknown = null;
+var never;
+
+console.log('value of Full Name:', fullName);
+console.log('value of Is Cool:', isCool);
+console.log('value of Total Pets:', totalPets);
+console.log('typeof fullName:', typeof 'Sara Roohan');
+console.log('typeof isCool:', typeof true);
+console.log('typeof totalPets:', typeof 0);
+console.log('value of Unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+console.log('value of Never:', never);
+console.log('typeof never:', typeof never);
