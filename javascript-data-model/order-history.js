7d631ad5aebacd0c4efba610360bdf99bc80a3dd
Sara Roohan <sara.roohan@gmail.com>
2021-04-02 13:33:58 -0700

    Added items property to contain array of objects for each item.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -8,13 +8,16 @@ var orderHistory = [
     dateOrderPlaced: 'August 4, 2020',
     dateOrderDelivered: 'Aug 8, 2020',
     returnsClosed: 'Sep 7, 2020',
-    itemNumber: '11111',
-    category: 'Book',
-    subCategory: 'Adult - Educational',
-    title: 'JavaScript for impatient programmers',
-    author: 'Rauschmayer, Dr.Axel',
-    itemPrice: '$31.55',
-    quantity: '1'
+    items: [{
+      itemNumber: 11111,
+      category: 'Book',
+      subCategory: 'Adult - Educational',
+      title: 'JavaScript for impatient programmers',
+      author: 'Rauschmayer, Dr.Axel',
+      itemPrice: '$31.55',
+      quantity: 1
+    }
+    ]
   },
   {
     orderNumber: '113-9984268-1280257',
@@ -25,13 +28,16 @@ var orderHistory = [
     dateOrderPlaced: 'July 19, 2020',
     dateOrderDelivered: 'Jul 20, 2020',
     returnsClosed: 'Aug 19, 2020',
-    itemNumber: '22222',
-    category: 'Books',
-    subCategory: 'Adult - Educational',
-    title: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    itemPrice: '$41.33',
-    quantity: '1'
+    items: [{
+      itemNumber: 22222,
+      category: 'Books',
+      subCategory: 'Adult - Educational',
+      title: 'The Timeless Way of Building',
+      author: 'Alexander, Christopher',
+      itemPrice: '$41.33',
+      quantity: 1
+    }
+    ]
   },
   {
     orderNumber: '114-2875557-9059409',
@@ -42,13 +48,16 @@ var orderHistory = [
     dateOrderPlaced: 'July 4, 2020',
     dateOrderDelivered: 'Jul 7, 2020',
     returnsClosed: 'Aug 5, 2020',
-    itemNumber: '33333',
-    category: 'Gaming',
-    subCategory: 'Controllers',
-    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and no Lag-Gamecube Adapter',
-    author: null,
-    itemPrice: '$15.98',
-    quantity: '1'
+    items: [{
+      itemNumber: 33333,
+      category: 'Gaming',
+      subCategory: 'Controllers',
+      title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and no Lag-Gamecube Adapter',
+      author: null,
+      itemPrice: '$15.98',
+      quantity: 1
+    }
+    ]
   },
   {
     orderNumber: '113-2883177-2648248',
@@ -59,12 +68,23 @@ var orderHistory = [
     dateOrderPlaced: 'July 3, 2020',
     dateOrderDelivered: 'Jul 5, 2020',
     returnsClosed: 'Aug 4, 2020',
-    itemNumber: ['44444', '55555'],
-    category: ['Gaming', 'Books'],
-    subCategory: ['Controllers', 'Adult - Educational'],
-    title: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
-    author: [null, 'Faroult, Stephane'],
-    itemPrice: ['$94.95', '$33.99'],
-    quantity: ['1', '1']
+    items: [{
+      itemNumber: 44444,
+      subCategory: 'Controllers',
+      title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      author: null,
+      itemPrice: '$94.95',
+      quantity: 1
+    },
+    {
+      itemNumber: 55555,
+      category: 'Books',
+      subCategory: 'Adult - Educational',
+      title: 'The Art of Sql',
+      author: 'Faroult, Stephane',
+      itemPrice: '$33.99',
+      quantity: 1
+    }
+    ]
   }
 ];
