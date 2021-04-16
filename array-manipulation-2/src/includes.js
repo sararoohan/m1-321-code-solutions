61a2cafc7d5eb5a03bf10feeaaf601a4b5b04ed6
Sara Roohan <sara.roohan@gmail.com>
2021-04-15 17:02:56 -0700

    Completed array-manipulation-2 exercise.

---
+++ array-manipulation-2/src/includes.js
@@ -0,0 +1,9 @@
+/* exported includes */
+
+function includes(array, value) {
+  if (array.indexOf(value) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ array-manipulation-2/src/take-right.js
@@ -0,0 +1,6 @@
+/* exported takeRight */
+
+function takeRight(array, count) {
+  var newArr = array.slice(array.length - count);
+  return newArr;
+}
