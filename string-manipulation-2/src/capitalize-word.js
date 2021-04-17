864293998a2d9b361708f59be152c1e07b09a010
Sara Roohan <sara.roohan@gmail.com>
2021-04-16 17:45:45 -0700

    string-manipulation-2

---
+++ string-manipulation-2/src/capitalize-word.js
@@ -0,0 +1,8 @@
+/* exported capitalizeWord */
+
+function capitalizeWord(word) {
+  // if (word === 'javascript') {
+  //   return word[0].toUpperCase() + word.slice(1).toLowerCase() + word[4].toUpperCase() + word.slice(5).toLowerCase();
+  // } else {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
