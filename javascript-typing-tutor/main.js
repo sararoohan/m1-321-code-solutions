a252deb1260cde4bcab830ab1de1465e90275665
Sara Roohan <sara.roohan@gmail.com>
2021-04-27 14:17:18 -0700

    Completed javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,5 +1,8 @@
 var $char = document.querySelectorAll('.char');
-// console.dir($char);
+var $modal = document.querySelector('.modal-container');
+var $yesbutton = document.querySelector('.button.yes');
+var $nobutton = document.querySelector('.button.no');
+var $accuracyCount = document.querySelector('.accuracy-count');
 var i = 0;
 
 document.addEventListener('keydown', function (event) {
@@ -10,4 +13,17 @@ document.addEventListener('keydown', function (event) {
   } else {
     $char[i].className = 'char ' + 'incorrect';
   }
+  if (i === $char.length - 1) {
+    $modal.style.display = 'block';
+    $accuracyCount.textContent = 'Nice! You got ' + i + ' out of 30 correct.';
+  }
+});
+
+$yesbutton.addEventListener('click', function (event) {
+  location.reload();
+  return false;
+});
+
+$nobutton.addEventListener('click', function (event) {
+  $modal.style.display = 'none';
 });
