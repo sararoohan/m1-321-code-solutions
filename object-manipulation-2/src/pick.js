dc71b7a70f7350a061502d79f9e3477dc9ad7e6f
Sara Roohan <sara.roohan@gmail.com>
2021-04-29 19:39:59 -0700

    Modified object-manipulations-2

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,5 +2,11 @@
 
 // function pick(source, keys) {
 //   var newObj = {};
-
-// }
+//   for (key in source) {
+//     if (keys.includes(key)) {
+//       // console.log(newObj[key] = source[key]);
+//       newObj[key] = source[key];
+//     }
+//   }
+//   return newObj;
+// };
