c3ecad98162fd118c655450f7e2734d7b4999008
Sara Roohan <sara.roohan@gmail.com>
2021-04-29 21:46:03 -0700

    Added solution for dom-event-delegation.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -7,3 +7,20 @@ document.querySelector('.task-list').addEventListener('click', function (event) 
     event.target.closest('.task-list-item').remove();
   }
 });
+
+// Solution
+// var $taskList = document.querySelector('.task-list');
+
+// $taskList.addEventListener('click', function (event) {
+//   console.log('event.target:', event.target);
+//   console.log('event.target.tagName:', event.target.tagName);
+
+// if (event.target.tagName !== 'BUTTON') {
+//   return;
+// }
+
+// var $taskListItem = event.target.closest('.task-liks-item');
+//   console.log('closest .task-list-item:', $taskListItem);
+//   $taskListItem.remove();
+
+// });
